b44c4b43263ad13e081ce2e77e4df2edf3fc4312
hirokazu oda <hirokazu@Hirokazu-no-MacBook-Air-2.local>
2015-12-07 01:46:34 +0900

    　

--- main.js
+++ main.js
@@ -41,6 +41,17 @@ function makeLikeFn() {
       el.style.left = getRandPer() + '%';
       return el;
     };
+    // 対象となるID名
+  var id = 'sound-file1' ;
+
+  // 初回以外だったら音声ファイルを巻き戻す
+  if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+  {
+    document.getElementById( id ).currentTime = 0;
+  }
+
+  // [ID:sound-file]の音声ファイルを再生[play()]する
+  document.getElementById( id ).play() ;
 }
 
 function getRandPer() {
